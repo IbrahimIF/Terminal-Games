9dd0e0da78718e0835a3cf9b5dbb0c9fdbace542
Ibrahim Farrah <ibrahimfarrah30@gmail.com>
2024-11-05 20:26:36 +0000

    Updated Rock-Paper-Scissors
    Changes:
    - imported emojis to be used in the rock-paperscissors.py game
    - Added tic-tac-toe.py file

---
+++ Rock-Paper-Scissors.py
@@ -0,0 +1,13 @@
+
+import sys
+sys.stdout.reconfigure(encoding='utf-8')
+
+# import emoji module 
+import emoji
+ 
+ 
+print(emoji.emojize("📃"))
+print(emoji.emojize("🧱"))
+print(emoji.emojize("✂️"))
+
+
---
+++ Tic-Tac-Toe.py
@@ -0,0 +1,1 @@
+# a tic tac toe game plaed int he terminal against an AI
